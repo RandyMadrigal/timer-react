63401deda942720b621ad6ee83b9098ba45bfd5b
Randy Madrigal <randymadrigalx12@gmail.com>
2024-08-27 22:10:26 -0400

    remove test file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <>
-      <h1 className="text-4xl md:text-5xl text-center mb-5">25 + 5 Clock</h1>
+      <h1 className="text-4xl md:text-5xl text-center mb-5">Pomodoro Clock</h1>
     </>
   );
 };
